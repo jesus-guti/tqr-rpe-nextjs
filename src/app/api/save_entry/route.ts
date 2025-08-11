d68eee0e45bf02b4089fab11f473d1d5efad41e6
jesus-guti <jesusgutierrezsiliceo@gmail.com>
2025-08-11 14:13:47 +0200

    feat: configure runtime and dynamic settings for save_entry API route to enhance performance

--- src/app/api/save_entry/route.ts
+++ src/app/api/save_entry/route.ts
@@ -1,3 +1,5 @@
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { GoogleSheetsService } from "@/lib/google-sheets";
